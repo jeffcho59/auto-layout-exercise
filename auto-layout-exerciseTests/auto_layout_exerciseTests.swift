bfb2b8ec79a83512568f2207e25bf2829e55ff86
jeffrey <jeffcho59@gmail.com>
2015-11-03 14:16:48 -0500

    Initial Commit

---
+++ auto-layout-exerciseTests/auto_layout_exerciseTests.swift
@@ -0,0 +1,36 @@
+//
+//  auto_layout_exerciseTests.swift
+//  auto-layout-exerciseTests
+//
+//  Created by Jeffrey Cho on 11/3/15.
+//  Copyright © 2015 personal. All rights reserved.
+//
+
+import XCTest
+@testable import auto_layout_exercise
+
+class auto_layout_exerciseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
